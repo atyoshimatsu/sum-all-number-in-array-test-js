d7c541aabe05f0c81cc557e1b05830d3cfc10144
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-24 02:10:21 +0000

    implemented sumItems function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  const tailElment = array.pop();
+  if (Array.isArray(tailElment)) {
+    return sumItems(array) + sumItems(tailElment);
+  }
+
+  return sumItems(array) + tailElment;
+};
 
 module.exports = sumItems;
